f2224f470f1de13da9dcd91d955adde4faced050
Mickey Ma <mickeyma222@gmail.com>
2024-03-09 16:29:14 +1100

    update

--- java-app/src/main/java/com/example/demo/controller/RepositoryDetailsController.java
+++ java-app/src/main/java/com/example/demo/controller/RepositoryDetailsController.java
@@ -1,4 +1,4 @@
-// package com.micky.demo.controller;
+package com.example.demo;.controller;
 
 import java.io.IOException;
 import java.util.HashMap;
--- java-app/src/test/java/com/example/demo/RepositoryDetailsControllerTest.java
+++ java-app/src/test/java/com/example/demo/RepositoryDetailsControllerTest.java
@@ -1,4 +1,4 @@
-// package com.micky.demo;
+package com.example.demo;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
